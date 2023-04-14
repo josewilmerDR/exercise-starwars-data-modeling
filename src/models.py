d93d3ab412ed35d1a8a0edd8e780e10c76bd9a83
josewilmerDR <josewdr@gmail.com>
2023-04-14 03:35:38 +0000

    update

--- src/models.py
+++ src/models.py
@@ -33,10 +33,6 @@ Base = declarative_base()
 
 
 
-
-
-
-# Definición de la clase Character
 class Character(Base):
     __tablename__ = 'characters'
     uid = Column(Integer, primary_key=True)
@@ -49,6 +45,7 @@ class Character(Base):
     eyes_color=Column(String(50), nullable=False)
     birth_year = Column(Float(), nullable=False)
     gender=Column(String(50), nullable=False)
+    users_favorited = relationship("Favorite_Character", back_populates="character")
 
 class Planet(Base):
     __tablename__ = 'planets'
@@ -61,6 +58,7 @@ class Planet(Base):
     gravity = Column(Float(), nullable=False)
     Population = Column(Float(), nullable=False)
     climate = Column(String, nullable=False)
+    users_favorited = relationship("Favorite_Planet", back_populates="planet")
 
 class Vehicles(Base):
     __tablename__ = 'vehicles'
@@ -73,8 +71,8 @@ class Vehicles(Base):
     cost_in_credits = Column(Float(), nullable=False)
     passengers = Column(Integer, nullable=False)
     cargo_capacity = Column(Float(), nullable=False)
+    users_favorited = relationship("Favorite_Vehicles", back_populates="vehicle")
 
-# Tabla User
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -85,25 +83,127 @@ class User(Base):
     subscription_date = Column(String, nullable=False)
     birth_date = Column(Integer)
     country = Column(String)
+    favorite_characters = relationship("Favorite_Character", back_populates="user")
+    favorite_planets = relationship("Favorite_Planet", back_populates="user")
+    favorite_vehicles = relationship("Favorite_Vehicles", back_populates="user")
 
-# Tabla User_Character
 class Favorite_Character(Base):
     __tablename__ = 'favorite_characters'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     character_uid = Column(Integer, ForeignKey('characters.uid'), nullable=False)
+    user = relationship("User", back_populates="favorite_characters")
+    character = relationship("Character", back_populates="users_favorited")
 
-# Tabla User_Planet
 class Favorite_Planet(Base):
     __tablename__ = 'favorite_planet'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     planet_uid = Column(Integer, ForeignKey('planets.uid'), nullable=False)
+    user = relationship("User", back_populates="favorite_planets")
+    planet = relationship("Planet", back_populates="users_favorited")
 
-# Tabla User_Starship
 class Favorite_Vehicles(Base):
     __tablename__ = 'favorite_vehicles'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    starship_uid = Column(Integer, ForeignKey('vehicles.uid'), nullable=False)
+    vehicle_uid = Column(Integer, ForeignKey('vehicles.uid'), nullable=False)
+    user = relationship("User", back_populates="favorite_vehicles")
+    vehicle = relationship("Vehicles", back_populates="users_favorited")
 
+
+
+
+
+
+
+
+# # Definición de la clase Character
+# class Character(Base):
+#     __tablename__ = 'characters'
+#     uid = Column(Integer, primary_key=True)
+#     name = Column(String(50), nullable=False)
+#     url = Column(String(50), nullable=False)
+#     height = Column(Float(), nullable=False)
+#     mass = Column(Float(), nullable=False)
+#     hair_color=Column(String(50), nullable=False)
+#     skin_color=Column(String(50), nullable=False)
+#     eyes_color=Column(String(50), nullable=False)
+#     birth_year = Column(Float(), nullable=False)
+#     gender=Column(String(50), nullable=False)
+#     users_favorited = relationship("Favorite_Character", back_populates="character")
+
+
+# class Planet(Base):
+#     __tablename__ = 'planets'
+#     uid = Column(Integer, primary_key=True)
+#     name = Column(String, nullable=False)
+#     url = Column(String, nullable=False)
+#     diameter = Column(Float(), nullable=False)
+#     rotation_period = Column(Float(), nullable=False)
+#     orbital_period = Column(Integer, nullable=False)
+#     gravity = Column(Float(), nullable=False)
+#     Population = Column(Float(), nullable=False)
+#     climate = Column(String, nullable=False)
+#     users_favorited = relationship("Favorite_Planet", back_populates="planet")
+
+
+# class Vehicles(Base):
+#     __tablename__ = 'vehicles'
+#     uid = Column(Integer, primary_key=True)
+#     name = Column(String, nullable=False)
+#     url = Column(String, nullable=False)
+#     model = Column(String, nullable=False)
+#     vehicle_class = Column(String, nullable=False)
+#     Manufacturer = Column(String, nullable=False)
+#     cost_in_credits = Column(Float(), nullable=False)
+#     passengers = Column(Integer, nullable=False)
+#     cargo_capacity = Column(Float(), nullable=False)
+#     users_favorited = relationship("Favorite_Vehicles", back_populates="vehicle")
+
+
+# # Tabla User
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True)
+#     email = Column(String, unique=True, nullable=False)
+#     password = Column(String, nullable=False)
+#     first_name = Column(String, nullable=False)
+#     last_name = Column(String, nullable=False)
+#     subscription_date = Column(String, nullable=False)
+#     birth_date = Column(Integer)
+#     country = Column(String)
+#     favorite_characters = relationship("Favorite_Character", back_populates="user")
+#     favorite_planets = relationship("Favorite_Planet", back_populates="user")
+#     favorite_vehicles = relationship("Favorite_Vehicles", back_populates="user")
+
+
+
+# # Tabla User_Character
+# class Favorite_Character(Base):
+#     __tablename__ = 'favorite_characters'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+#     character_uid = Column(Integer, ForeignKey('characters.uid'), nullable=False)
+#     user = relationship("User", back_populates="favorite_characters")
+#     character = relationship("Character", back_populates="users_favorited")
+
+# # Tabla User_Planet
+# class Favorite_Planet(Base):
+#     __tablename__ = 'favorite_planet'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+#     planet_uid = Column(Integer, ForeignKey('planets.uid'), nullable=False)
+#     user = relationship("User", back_populates="favorite_planets")
+#     planet = relationship("Planet", back_populates="users_favorited")
+
+
+# # Tabla User_Starship
+# class Favorite_Vehicles(Base):
+#     __tablename__ = 'favorite_vehicles'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+#     starship_uid = Column(Integer, ForeignKey('vehicles.uid'), nullable=False)
+#     user = relationship("User", back_populates="favorite_vehicles")
+#     vehicle = relationship("Vehicles", back_populates="users_favorited")
+    
